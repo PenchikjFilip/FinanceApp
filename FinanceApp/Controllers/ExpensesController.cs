8bf21d6b23bc0f9bf448bd35a14045cee0254105
Filip Penchikj <filip.pencic@gmail.com>
2026-03-09 23:58:26 +0100

    feat: add Create GET action to display form

--- FinanceApp/Controllers/ExpensesController.cs
+++ FinanceApp/Controllers/ExpensesController.cs
@@ -16,5 +16,10 @@ namespace FinanceApp.Controllers
             var expenses = _context.Expenses.ToList();
             return View(expenses);
         }
+        public IActionResult Create()
+        {
+            return View();
+        }
+
     }
 }
