b7dd64804c73f6e4b35c70a91a6b8dc07ee78463
Filip Penchikj <filip.pencic@gmail.com>
2026-03-10 00:01:10 +0100

    feat: implement Create POST action to save expenses

--- FinanceApp/Controllers/ExpensesController.cs
+++ FinanceApp/Controllers/ExpensesController.cs
@@ -20,6 +20,16 @@ namespace FinanceApp.Controllers
         {
             return View();
         }
-
+        [HttpPost]
+        public IActionResult Create(Expense expense)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Expenses.Add(expense);
+                _context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(expense);
+        }
     }
 }
