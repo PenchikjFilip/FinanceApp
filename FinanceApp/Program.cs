ac7e24cec8baef211b552518a0d0a26e36cc9dab
Filip Penchikj <filip.pencic@gmail.com>
2026-03-09 23:12:26 +0100

    feat: configure SQL Server connection and register DbContext

--- FinanceApp/Program.cs
+++ FinanceApp/Program.cs
@@ -1,7 +1,16 @@
+using FinanceApp.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+
+builder.Services.AddDbContext<FinanceAppContext>(options =>
+    options.UseSqlServer(connectionString));
+
 
 var app = builder.Build();
 
