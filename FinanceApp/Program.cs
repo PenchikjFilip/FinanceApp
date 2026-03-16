3c51a1174259dabc3dda901c5e642fb73dba62d7
Filip Penchikj <filip.pencic@gmail.com>
2026-03-16 19:12:21 +0100

    feat: register ExpensesService as scoped service

--- FinanceApp/Program.cs
+++ FinanceApp/Program.cs
@@ -1,4 +1,5 @@
 using FinanceApp.Data;
+using FinanceApp.Data.Service;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -11,6 +12,7 @@ var connectionString = builder.Configuration.GetConnectionString("DefaultConnect
 builder.Services.AddDbContext<FinanceAppContext>(options =>
     options.UseSqlServer(connectionString));
 
+builder.Services.AddScoped<IExpensesService, ExpensesService>();    
 
 var app = builder.Build();
 
